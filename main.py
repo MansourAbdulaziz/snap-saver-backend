f989a3e7a68d51f3f6ff5926f73a49d66e03ae67
MansourAbdulaziz <alkathieris@gmail.com>
2025-06-29 08:40:19 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse
 from scraper import extract_instagram_video
+from fastapi import Request
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Snap Saver API (Pyppeteer) is running"}
-
-@app.get("/download")
-async def download_instagram_video(url: str = Query(...)):
+@app.post("/api/instagram")
+async def post_instagram_video(request: Request):
     try:
+        body = await request.json()
+        url = body.get("url")
+        if not url:
+            return JSONResponse(status_code=400, content={"error": "Missing URL"})
+
         video_url = await extract_instagram_video(url)
-        return {"success": True, "video_url": video_url}
+        return {"download_url": video_url}
     except Exception as e:
-        return JSONResponse(status_code=500, content={"success": False, "error": str(e)})
+        return JSONResponse(status_code=500, content={"error": str(e)})
