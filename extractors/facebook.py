2a24a0c0f0ca2ff5697958f995a6fb29930d956f
MansourAbdulaziz <alkathieris@gmail.com>
2025-06-30 08:54:58 +0300

    Update facebook.py

--- extractors/facebook.py
+++ extractors/facebook.py
@@ -1,11 +1,12 @@
+import asyncio
 from pyppeteer import launch
 
 async def extract_facebook_video(url: str) -> str:
-    browser = await launch(headless=True, args=["--no-sandbox"])
+    browser = await launch(headless=True, args=["--no-sandbox", "--disable-setuid-sandbox"])
     page = await browser.newPage()
-    await page.goto("https://snapsave.app/facebook-video-downloader", timeout=60000)
-
-    await page.waitForSelector("#s_input")
+    await page.goto("https://snapsave.app/facebook-downloader", timeout=60000)
+    
+    await page.waitForSelector("#s_input", timeout=60000)
     await page.type("#s_input", url)
     await page.click("#submit")
 
@@ -19,7 +20,7 @@ async def extract_facebook_video(url: str) -> str:
         )
     except Exception:
         await browser.close()
-        raise Exception("❌ لم يتم العثور على رابط فيسبوك داخل الصفحة")
+        raise Exception("❌ لم يتم العثور على رابط التحميل داخل الصفحة")
 
     element = await page.querySelector("a.button.is-success")
     download_url = await page.evaluate('(el) => el.href', element)
