31d1095754ddf1f58760e38a925326d7c4e4e3e5
MansourAbdulaziz <alkathieris@gmail.com>
2025-06-30 08:20:39 +0300

    Update youtube.py

--- extractors/youtube.py
+++ extractors/youtube.py
@@ -1,13 +1,16 @@
 import yt_dlp
 
-async def extract_youtube_video(url):
+async def extract_youtube_video(url: str) -> str:
     ydl_opts = {
         'quiet': True,
-        'no_warnings': True,
-        'format': 'best',
+        'skip_download': True,
+        'format': 'mp4',
         'forcejson': True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
-        return info_dict.get("url")
+        try:
+            info = ydl.extract_info(url, download=False)
+            return info['url']
+        except Exception:
+            raise Exception("❌ فشل استخراج رابط YouTube")
